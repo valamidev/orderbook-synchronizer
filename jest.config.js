3d9fed9fbb4c23349e859ded12123a0a92180db5
T <52665225+valamidev@users.noreply.github.com>
2020-03-24 18:53:54 +0100

    :up: Add coveralls support

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  globals: {
+    'ts-jest': {
+      diagnostics: {
+        ignoreCodes: [2532],
+      },
+    },
+  },
+
+  collectCoverage: true,
+
+  coveragePathIgnorePatterns: ['/node_modules|lib/'],
+
+  collectCoverageFrom: ['src/**/*.ts'],
+
+  transform: {
+    '.(ts|tsx)': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+};
